e6ec4e4ebb0a2cb9b3cef3958f10743f898860dc
Sumit Desale <sumitdesale706@gmail.com>
2026-09-08 00:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,27 +5,15 @@ let button = document.querySelector("#add");
 let table = document.querySelector("#t");
 let grandTotal = document.querySelector("#total");
 
-var nameToAdd;
-var priceToAdd = 0;
 var sum = 0;
 
-
-name.addEventListener("change", () => {
-	 nameToAdd = name.value;
-});
-
-price.addEventListener("change", () => {
-	 priceToAdd = Number(price.value);
-});
-
-
 function addRow(){
-	   if(name.value && price.value){
+	   if(name.value !== "" && price.value !== ""){
 		   var row = document.createElement("tr");
            var col1 = document.createElement("td");
            var col2 = document.createElement("td");
-		   col1.innerText = nameToAdd;
-	       col2.innerText = priceToAdd;
+		   col1.innerText = name.value;
+	       col2.innerText = Number(price.value);
 	       row.appendChild(col1);
 	       row.appendChild(col2);
 	       table.appendChild(row);
@@ -33,8 +21,8 @@ function addRow(){
 }
 
 function findTotal(){
-	  if(priceToAdd){
-		   sum = sum + priceToAdd;
+	  if(price.value !== ""){
+		   sum = sum + Number(price.value);
 	       grandTotal.innerText = `GrandTotal=${sum}`;
 	  }
 }
@@ -45,6 +33,9 @@ function emptyAll(){
 }
 
 button.addEventListener("click", () => {
+	   if(name.value === "" && price.value === ""){
+		   return;
+	   }
 	   addRow();
 	   findTotal();
        emptyAll(); 
